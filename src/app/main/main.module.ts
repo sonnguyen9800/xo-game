82daf7946167a107e569ed824ad9bb59c34db55a
sonnguyen9800 <sonnguyen9800@gmail.com>
2020-05-24 19:59:56 +0700

    New module added

---
+++ src/app/main/main.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MainComponent }  from './main.component';
+
+
+
+@NgModule({
+  declarations: [
+    MainComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    MainComponent
+  ]
+})
+export class MainModule { }
