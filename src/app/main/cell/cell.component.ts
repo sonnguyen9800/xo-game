3c33aa2c8860a5293bec1fc81b2ff280e7db30aa
sonnguyen9800 <sonnguyen9800@gmail.com>
2020-07-02 09:33:37 +0700

    Drawing on Cell, Game-Table

--- src/app/main/cell/cell.component.ts
+++ src/app/main/cell/cell.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Cell } from '../cell/cell.interface';
-
+import { Player } from '../../Player';
 @Component({
   selector: 'app-cell',
   templateUrl: './cell.component.html',
@@ -9,7 +9,7 @@ import { Cell } from '../cell/cell.interface';
 
 export class CellComponent implements OnInit, Cell {
 
-
+  playerInstance = Player
   // state: represent the state of each cell.
   // 0: blank cell; 1: first-person marked; 2: second-person   
   @Input('row') row: number;
